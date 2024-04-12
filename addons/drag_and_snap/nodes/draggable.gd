7b56b7d798dbe73ecbe65ec3cda8f959b8762b74
ctkerns <chmos13@yahoo.com>
2024-04-11 23:52:10 -0700

    Freeze RigidBody while dragging

--- addons/drag_and_snap/nodes/draggable.gd
+++ addons/drag_and_snap/nodes/draggable.gd
@@ -2,12 +2,13 @@
 extends Node
 class_name Draggable
 
-# Snapping.
+# Save state when snapping.
 var _original_parent
 var _original_rotation
 var _snapped_to = null
 
 func _enter_tree():
+	# Ensure component is used correctly.
 	update_configuration_warnings()
 
 	assert(get_parent() != get_tree().get_root(), "ERROR: Draggable is a component and must be a child of another node.")
@@ -17,13 +18,14 @@ func _enter_tree():
 		get_parent().connect("input_event", Callable(self, "_on_input_event"))
 
 func _ready():
-	# At run time, a draggable object can not be the scene root.
+	# Ensure component is used correctly.
 	assert(get_parent().get_parent() != get_tree().get_root(), "ERROR: At runtime, a draggable object must be a child of another node.")
 
-	# For snapping.
+	# Save original state before any snapping occurs.
 	_original_parent = get_parent().get_parent()
 	_original_rotation = get_parent().get_rotation()
 
+# Ensure component is used correctly.
 func _get_configuration_warnings():
 	var warnings = PackedStringArray()
 	
@@ -44,6 +46,9 @@ func _on_input_event(camera, event, _position, _normal, _shape_idx):
 	# Start a new drag.
 	if event is InputEventMouseButton and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		DragSnap.start_drag(camera, get_parent())
+
+		if get_parent() is RigidBody3D:
+			get_parent().set_freeze_enabled(true)
 
 func is_snapped():
 	return _snapped_to != null
